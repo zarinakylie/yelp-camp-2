151d1a88bfddac349f06c4859a24bd3489d36900
Zarina Kylie <zarinakylielagera@gmail.com>
2026-06-10 00:49:12 +0800

    changed yelpcamp to kyliecamp

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const User = require('../models/user');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -15,11 +18,12 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
+    const user = await User.findOne();
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * cities.length);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '5f5c330c2cd79d538f2c66d9',
+            author: user._id,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             geometry: {
             type: "Point",
